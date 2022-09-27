54e4e757ef652f32c64b3a7f62b8dac676e17c2a
Matt Barletta <102425378+letta-9@users.noreply.github.com>
2022-09-27 08:26:21 -0400

    No calcs done on server file. Only creating table

--- server.R
+++ server.R
@@ -12,42 +12,11 @@ library(data.table)
 # IMPORT CSV FILES #
 ####################
 
+prospects <- read_csv('prospects_clean.csv') #From fangraphs the board / mlb.com. Cleaned in prospects.R
+mlb_bios <- read_csv('mlb_bios_clean.csv') #From baseballr. Cleaned in batters.R
+batters <- read_csv('batters_clean.csv') #From baseballr. Cleaned in batters.R
+pitchers <- read_csv('pitchers_clean.csv') #Raw CSV exported from baseball savant pitch arsenal page. baseballr for pitch movement. Cleaned in pitchers.R
 
-prospects <- read_csv("prospect_master_090522.csv") #From fangraphs the board / mlb.com
-mlb_bios <- read_csv("mlb_bios.csv") #From data.R
-batters <- read_csv("mlb_batters.csv") #From data.R
-pros_cat <- read.csv('prospect_catchers.csv')
-
-
-
-########################
-# CLEAN PROSPECTS DATA #
-########################
-
-
-prospects$Age <- floor(prospects$Age)
-
-prospects$W <- plyr::round_any(prospects$W, 5, f=ceiling)
-prospects$Class <- ifelse(prospects$Pos == 'SP' | prospects$Pos == 'MIRP'| prospects$Pos == 'SIRP', 'Pitcher', 'Position')
-
-tool_list <- c('Hit','Game','Raw','Spd','Fld','FB','SL','CB','CH','CMD')
-for (a in tool_list){
-  prospects <- prospects %>% separate(a, c(paste('c',a),a), " / ")
-}
-
-
-prospects$Hit <-as.numeric(prospects$Hit)
-prospects$Hit <- plyr::round_any(prospects$Hit, 10, f = ceiling)
-prospects$Game <-as.numeric(prospects$Game)
-prospects$Game <- plyr::round_any(prospects$Game, 10, f = ceiling)
-prospects$Raw <-as.numeric(prospects$Raw)
-prospects$Raw <- plyr::round_any(prospects$Raw, 10, f = ceiling)
-prospects$Spd <-as.numeric(prospects$Spd)
-prospects$Spd <- plyr::round_any(prospects$Spd, 10, f = ceiling)
-prospects$Fld <-as.numeric(prospects$Fld)
-prospects$Fld <- plyr::round_any(prospects$Fld, 10, f = ceiling)
-
-pros_cat <- pros_cat %>% filter(Pos == 'C') %>% select(Name, Pos, Arm)
 
 ############################################
 # CREATE DATA TABLE FOR FRONT PAGE DISPLAY #
@@ -55,39 +24,9 @@ pros_cat <- pros_cat %>% filter(Pos == 'C') %>% select(Name, Pos, Arm)
 
 display_prospects <- prospects %>% select(Top100, Name, Pos, Org, Age, H, W, B, T)
 
-#########################
-# CREATE COMP VARIABLES #
-#########################
-
-pros_body_comp <- prospects %>% select(Name, Class, H, W)
-mlb_body_comp <- mlb_bios %>% select(Name, Class, H, W)
-body_comp <- merge(pros_body_comp, mlb_body_comp, by = c('Class','H','W'), all=TRUE)
-body_comp <- subset(body_comp, !is.na(Name.x))
-body_comp[is.na(body_comp)] <- 'No Simliar Player'
-
-
-pros_hit_comp <- prospects %>% select(Name, Hit)
-mlb_hit_comp <- batters %>% select(Name, Hit)
-hit_comp <- merge(pros_hit_comp, mlb_hit_comp, by = 'Hit', all=TRUE)
-
-pros_game_comp <- prospects %>% select(Name, Game)
-mlb_game_comp <- batters %>% select(Name, Game)
-game_comp <- merge(pros_game_comp, mlb_game_comp, by = 'Game', all=TRUE)
- 
-pros_raw_comp <- prospects %>% select(Name, Raw)
-mlb_raw_comp <- batters %>% select(Name, Raw)
-raw_comp <- merge(pros_raw_comp, mlb_raw_comp, by = 'Raw', all=TRUE)
- 
-pros_spd_comp <- prospects %>% select(Name, Spd)
-mlb_spd_comp <- batters %>% select(Name, Spd)
-spd_comp <- merge(pros_spd_comp, mlb_spd_comp, by = 'Spd', all=TRUE)
-
-pros_fld_comp <- prospects %>% select(Name, Pos, Fld)
-mlb_fld_comp <- batters %>% select(Name, Pos, Fld)
-fld_comp <- merge(pros_fld_comp, mlb_fld_comp, by = c('Pos','Fld'), all=TRUE)
-
-
-
+#################
+# SHINY SERVER #
+################
 
 library(shiny)
 library(readr)
@@ -103,65 +42,46 @@ shinyServer(function(input, output){
     selection = "single",
   )
   
-
   selected_player = reactive({
     x = isolate(display_prospects)
     x = x[input$display_prospects_rows_selected,]
     x$Name
   })
   
-  selected_body = reactive({
-    body_comp %>% filter(Name.x == selected_player())
-  })
   
-  selected_hit = reactive({
-    hit_comp %>% filter(Name.x == selected_player())
-  })
-  
-  selected_game = reactive({
-    game_comp %>% filter(Name.x == selected_player())
-  })
-  
-  selected_raw = reactive({
-    raw_comp %>% filter(Name.x == selected_player())
-  })
-  
-  selected_spd = reactive({
-    spd_comp %>% filter(Name.x == selected_player())
-  })
-  
-  selected_fld = reactive({
-    fld_comp %>% filter(Name.x == selected_player())
-  })
-  
-
   modal_display = reactive({
-    body = selected_body()[1,]
-    body$Cat <- "Body Type"    
-    body = body %>% select(Cat,Name.x,Name.y)
-    
-    hit = selected_hit()[1,]
-    hit$Cat <- "Hit"    
-    hit = hit %>% select(Cat,Name.x,Name.y)
+    modal <- data.frame(matrix(ncol=13, nrow=1))
+    colnames(modal) <- c('Name', 'Body', 'Hit', 'Game', 'Raw', 'Spd', 'Fld', 'Arm', 'FB', 'SL', 'CB', 'CH','CMD')
 
-    game = selected_game()[1,]
-    game$Cat <- "Game Power"    
-    game = game %>% select(Cat,Name.x,Name.y)
-    
-    raw = selected_raw()[1,]
-    raw$Cat <- "Raw Power"    
-    raw = raw %>% select(Cat,Name.x,Name.y)    
-    
-    spd = selected_spd()[1,]
-    spd$Cat <- "Speed"    
-    spd = spd %>% select(Cat,Name.x,Name.y)
-    
-    fld = selected_fld()[1,]
-    fld$Cat <- "Fielding"
-    fld = fld %>% select(Cat,Name.x,Name.y)
-    
-    display <- rbind(body, hit, game, raw, spd, fld)
-    
+    selected_data <- prospects %>% filter(Name == selected_player())
+
+    modal$Name <- selected
+
+    if (selected_data$Class == 'Position'){
+      modal$Hit <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Hit','B')),1), Name.y)
+      modal$Game <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Game','B')),1), Name.y)
+      modal$Raw <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Raw','B')),1), Name.y)
+      modal$Spd <- dplyr::select(sample_n(merge(selected_data, batters, by=('Spd')),1), Name.y)
+      modal$Fld <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Pos','Fld')),1), Name.y)
+    } 
+
+    if (selected_data$Class == 'Catcher'){
+      modal$Hit <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Hit','B')),1), Name.y)
+      modal$Game <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Game','B')),1), Name.y)
+      modal$Raw <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Raw','B')),1), Name.y)
+      modal$Spd <- dplyr::select(sample_n(merge(selected_data, batters, by=('Spd')),1), Name.y)
+      modal$Arm <- dplyr::select(sample_n(merge(selected_data, batters, by=c('Pos','Arm')),1), Name.y)
+    } 
+
+    if (selected_data$Class == 'Pitcher'){
+      modal$FB <- dplyr::select(sample_n(merge(selected_data, pitchers, by=c('T','FB')),1), Name.y)
+      modal$SL <- dplyr::select(sample_n(merge(selected_data, pitchers, by=c('T','SL')),1), Name.y)
+      modal$CB <- dplyr::select(sample_n(merge(selected_data, pitchers, by=c('T','CB')),1), Name.y)
+      modal$CH <- dplyr::select(sample_n(merge(selected_data, pitchers, by=c('T','CH')),1), Name.y) 
+    } 
+
+    modal <- t(modal)
+    modal <- na.omit(modal)
   })
 
   
