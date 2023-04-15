dd0ac2f23b46f31c38fb2505b0f8e5a97b440f99
Matt Barletta <102425378+letta-9@users.noreply.github.com>
2023-04-15 15:59:31 -0400

    Added Has.FB Columns

--- data/2.0/pros_pitchers.R
+++ data/2.0/pros_pitchers.R
@@ -22,148 +22,19 @@ for (i in 3:7){
   pros_pitchers[,i] <- plyr::round_any(pros_pitchers[,i], 10, f = ceiling)
 }
 
+pros_pitchers <- pros_pitchers %>%
+  mutate(
+    FB.Type = case_when(
+      FB.Type == 'Sink' | FB.Type == 'Sink/Tail' ~ 'Sinker',
+      FB.Type == 'Cut' | FB.Type == 'Cut/Rise' | FB.Type == 'Cut/Downhi' | FB.Type == 'Cut+Sink'  ~ 'Cutter',
+    )
+  )
 
+pros_pitchers$FB.Type[is.na(pros_pitchers$FB.Type)] <- '4-Seamer'
 
+pros_pitchers$Has.FB <- !is.na(pros_pitchers$FB)
+pros_pitchers$Has.SL <- !is.na(pros_pitchers$SL)
+pros_pitchers$Has.CB <- !is.na(pros_pitchers$CB)
+pros_pitchers$Has.CH <- !is.na(pros_pitchers$CH)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #######################
-# # CLEAN PROSPECT DATA #
-# #######################
-# 
-# prospects$Age <- floor(prospects$Age)
-# 
-# prospects$Class <- ifelse(prospects$Pos == 'SP' | prospects$Pos == 'MIRP'| prospects$Pos == 'SIRP', 'Pitcher', 'Position')
-# 
-# tool_list <- c('Hit','Game','Raw','Spd','Fld','FB','SL','CB','CH','CMD')
-# for (a in tool_list){
-#   prospects <- prospects %>% separate(a, c(paste('c',a),a), " / ")
-# }
-# 
-# prospects <- merge(prospects, pros_arm, by='Name')
-# 
-# prospects$Hit <-as.numeric(prospects$Hit)
-# prospects$Hit <- plyr::round_any(prospects$Hit, 10, f = ceiling)
-# prospects$Game <-as.numeric(prospects$Game)
-# prospects$Game <- plyr::round_any(prospects$Game, 10, f = ceiling)
-# prospects$Raw <-as.numeric(prospects$Raw)
-# prospects$Raw <- plyr::round_any(prospects$Raw, 10, f = ceiling)
-# prospects$Spd <-as.numeric(prospects$Spd)
-# prospects$Spd <- plyr::round_any(prospects$Spd, 10, f = ceiling)
-# prospects$Fld <-as.numeric(prospects$Fld)
-# prospects$Fld <- plyr::round_any(prospects$Fld, 10, f = ceiling)
-# prospects$Arm <-as.numeric(prospects$Arm)
-# prospects$Arm <- plyr::round_any(prospects$Arm, 10, f = ceiling)
-# prospects$FB <-as.numeric(prospects$FB)
-# prospects$FB <- plyr::round_any(prospects$FB, 10, f = ceiling)
-# prospects$SL <-as.numeric(prospects$SL)
-# prospects$SL <- plyr::round_any(prospects$SL, 10, f = ceiling)
-# prospects$CB <-as.numeric(prospects$CB)
-# prospects$CB <- plyr::round_any(prospects$CB, 10, f = ceiling)
-# prospects$CH <-as.numeric(prospects$CH)
-# prospects$CH <- plyr::round_any(prospects$CH, 10, f = ceiling)
-# prospects$CMD <-as.numeric(prospects$CMD)
-# prospects$CMD <- plyr::round_any(prospects$CMD, 10, f = ceiling)
-# 
-# prospects$Class[prospects$Pos.x == 'C'] <- 'Catcher'
-# 
-# prospects <- prospects %>% select(c(1,2,3,39,4,5,6,7,8,9,11,15,17,19,21,45,24,26,28,30,32))
-# colnames(prospects)[3] <- 'Pos'
-# colnames(prospects)[6] <- 'Age'
-# 
-# prospects <- cbind(prospects, prospects$Class)
-# colnames(prospects)[22] <- 'Bio.Class'
-# prospects$Bio.Class[prospects$Bio.Class == 'Catcher'] <- 'Position'
-# 
-# prospects <-cbind(prospects, prospects$H)
-# prospects <-cbind(prospects, prospects$W)
-# prospects <- prospects %>% separate(H, c('Ft','In'), "' ")
-# prospects$In <- sub('"','',prospects$In)
-# prospects$Ft <- as.numeric(prospects$Ft)
-# prospects$In <- as.numeric(prospects$In)
-# prospects$H <- (prospects$Ft * 12) + prospects$In
-# prospects <- prospects %>% relocate(H, .before = W)
-# 
-# 
-# 
-# for (i in 9:10){
-#   body_std <- sd(prospects[,i])
-#   body_mean <- mean(prospects[,i])
-#   
-#   body_breaks = c((body_mean - (2*body_std)),(body_mean - body_std), body_mean, (body_mean + body_std),(body_mean + (2*body_std)))
-#   
-#   t <- findInterval(prospects[,i], body_breaks)
-#   t <- factor(t)
-#   
-#   levels(t) <- c(30,40,50,60,70,80)
-#   
-#   prospects <- cbind(prospects, t)
-# }
-# 
-# prospects <- prospects[c(1,2,3,4,5,6,27,28,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26)]
-# colnames(prospects)[7] <- 'H'
-# colnames(prospects)[8] <- 'W'
-# colnames(prospects)[23] <- 'Ht'
-# colnames(prospects)[24] <- 'Wt'
-# 
-# #####################
-# #Add Pos Class column
-# #####################
-# 
-# prospects <- prospects %>% 
-#   mutate(
-#     Pos.Class = case_when(
-#       Pos == 'CF' | Pos == 'LF' | Pos == 'RF' ~ 'OF',
-#       Pos == '2B' | Pos == 'SS' ~ 'MI',
-#       Pos == '3B' | Pos == '1B' ~ 'CI',      
-#       Pos == 'C' ~ 'C',
-#       Pos == 'DH' ~ 'DH',
-#       Pos == 'SP' ~ 'SP',
-#       Pos == 'MIRP' | Pos == 'SIRP' ~ 'RP',
-#     )
-#   )
-# 
-# #####################
-# #Add Pos Archetype column
-# #####################
-# 
-# prospects$Arch <- NA
-# 
-# prospects <- prospects %>% 
-#   mutate(
-#     Arch = case_when(
-#       Class == 'Pitcher' & FB >= 70 & CMD >= 40  ~ 'VELO',
-#       Class == 'Pitcher' & FB < 70 & CMD >= 60  ~ 'CTRL',
-#       Class == 'Pitcher' & FB < 70 & (SL >= 70 | CB >= 70 | CH >= 70)  ~ 'BREAK',
-#       Class == 'Pitcher' & FB < 70 & CMD < 40 ~ 'B-WLD',
-#       Class == 'Pitcher' & FB >= 70 & CMD < 40 ~ 'V-WLD',
-#       Class == 'Pitcher' & is.na(Arch) ~ 'BLNC',
-#       Bio.Class == 'Position' & Game >= 60 & (Game - Hit) >= 20  ~ 'PWR',
-#       Bio.Class == 'Position' & (Hit - Game) >= 20  ~ 'CON',
-#       Bio.Class == 'Position' & Fld > Hit & Fld > Game & Fld > Raw & Hit < 50 & Game < 50 ~ 'FLD',
-#       Bio.Class == 'Position' & Spd >= 70 & Hit < 50 & Game < 50 ~ 'SPD',
-#       Bio.Class == 'Position' & Hit >= 60 & Game >= 60 & Fld >= 60 & Arm >= 60 & Fld >= 60 ~ '5-T',
-#       Bio.Class == 'Position' & is.na(Arch) ~ 'BLNC'
-#     )
-#   )
-# 
-# 
-# write_csv(prospects, 'prospects_clean.csv') #Move to app folder
-# 
+pros_pitchers <- pros_pitchers %>% mutate_all(as.character)
