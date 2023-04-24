a03166a75094c6003b30d1e193d0307d7039386c
Matt Barletta <102425378+letta-9@users.noreply.github.com>
2023-04-23 20:57:40 -0400

    Added archetype column

--- data/pros_pitchers.R
+++ data/pros_pitchers.R
@@ -5,7 +5,7 @@ library(tidyr)
 library(readr)
 
 # Imports
-pros_pitchers <- read.csv('csv/pros_pitchers_raw.csv')
+pros_pitchers <- read.csv('raw csv/pros_pitchers_raw.csv')
 
 # Clean
 pros_pitchers <- pros_pitchers %>% select(Name, FB.Type, FB, SL, CB, CH, CMD)
@@ -39,4 +39,26 @@ pros_pitchers$Has.CH <- !is.na(pros_pitchers$CH)
 
 pros_pitchers <- pros_pitchers %>% mutate_all(as.character)
 
-write_csv(pros_pitchers, 'csv/pros_pitchers_clean.csv')
+for (i in 3:7){
+  pros_pitchers[,i] <- as.numeric(as.character(pros_pitchers[,i]))
+}
+
+
+pros_pitchers$Arch <- NA
+
+pros_pitchers <- pros_pitchers %>%
+  mutate(
+    Arch = case_when(
+      FB >= 70 & CMD >= 40  ~ 'VELO',
+      FB < 70 & CMD >= 60  ~ 'CTRL',
+      FB < 70 & (SL >= 70 | CB >= 70 | CH >= 70)  ~ 'BREAK',
+      FB < 70 & CMD < 40 ~ 'B-WLD',
+      FB >= 70 & CMD < 40 ~ 'V-WLD',
+      is.na(Arch) ~ 'BLNC',
+    )
+  )
+
+pros_pitchers <- pros_pitchers %>% relocate(Arch, .before = FB.Type)
+
+
+write_csv(pros_pitchers, 'app/pros_pitchers_clean.csv')
