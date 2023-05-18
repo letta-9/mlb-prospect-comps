d0432364b48508af1c8b698faa11b4ab2a796e41
Matt Barletta <102425378+letta-9@users.noreply.github.com>
2023-05-17 22:20:24 -0400

    Display bar chart in modal

--- app/server.R
+++ app/server.R
@@ -1,10 +1,6 @@
 library(dplyr)
-#library(glue)
 library(readr)
-#library(plyr)
-#library(tidyr)
-#library(stringi)
-#library(data.table)
+library(ggplot2)
 
 ####################
 # IMPORT CSV FILES #
@@ -48,6 +44,10 @@ library(readr)
 shinyServer(function(input, output){
   
   
+  #######################
+  # VARIABLES
+  #######################
+  
   output$display_prospects <- renderDT(
     display_prospects,
     options = list(pageLength = 20),
@@ -55,65 +55,73 @@ shinyServer(function(input, output){
     selection = "single",
   )
   
-
   
   selected_player = reactive({
     x = isolate(display_prospects)
     x = x[input$display_prospects_rows_selected,]
     x = x$Name
+    print(x)
   })
-
+  
+  
+  selected_data = reactive({
+    y <- pros %>% filter(Name == selected_player())
+    
+    if (y$Pos.Class == 'Position'){
+      rownames(y) <- y$Name
+      y <- y %>% select(H, W, HIT, PWR, RAW, BAT_CTRL, DISC, FLD, SPD)
+    } else {
+      rownames(y) <- y$Name
+      y <- y %>% select(H, W, FB, SL, CB, CH, CMD)
+      y[is.na(y)] <- 0
+    }
+    print(y)
+  })
+  
+  
   
   mlb_comp = reactive({
-    selected_data <- pros %>% filter(Name == selected_player())
+    z <- pros %>% filter(Name == selected_player())
     
-    if (selected_data$Pos.Class == 'Position'){
-      comp_group <- mlb %>% filter(Pos.Class == selected_data$Pos.Class &
-                                     Pos.Group == selected_data$Pos.Group &
-                                     Arch == selected_data$Arch &
-                                     B == selected_data$B)
+    if (z$Pos.Class == 'Position'){
+      comp_group <- mlb %>% filter(Pos.Class == z$Pos.Class &
+                                     Pos.Group == z$Pos.Group &
+                                     Arch == z$Arch &
+                                     B == z$B)
       
       rownames(comp_group) <- comp_group$Name
       comp_group <- comp_group %>% select(H, W, HIT, PWR, RAW, BAT_CTRL, DISC, FLD, SPD)
-      
-      rownames(selected_data) <- selected_data$Name
-      selected_data <- selected_data %>% select(H, W, HIT, PWR, RAW, BAT_CTRL, DISC, FLD, SPD)
-      
     } else {
-      comp_group <- mlb %>% filter(Arch == selected_data$Arch &
-                                     T == selected_data$B &
-                                     FB.Type == selected_data$FB.Type)
+      comp_group <- mlb %>% filter(Arch == z$Arch &
+                                     T == z$B &
+                                     FB.Type == z$FB.Type)
       
       rownames(comp_group) <- comp_group$Name
       comp_group <- comp_group %>% select(H, W, FB, SL, CB, CH, CMD)
-      
       comp_group[is.na(comp_group)] <- 0
-      
-      rownames(selected_data) <- selected_data$Name
-      selected_data <- selected_data %>% select(H, W, FB, SL, CB, CH, CMD)
-      
-      selected_data[is.na(selected_data)] <- 0
-      
     }
     
     for (i in 1:nrow(comp_group)){
-      comp_group[i,] <- abs(comp_group[i,] - selected_data)
+      comp_group[i,] <- abs(comp_group[i,] - selected_data())
     }
     
     comp_group$Ovl <- rowSums(comp_group)
     comp_group$Name <- rownames(comp_group)
-    rownames(comp_group) <- NULL
-    comp_group <- comp_group %>% relocate(Name, .before = H)
     comp_group <- comp_group %>% arrange(Ovl)
+    rownames(comp_group) <- comp_group$Name
+    comp_group <- comp_group %>% select(H, W, HIT, PWR, RAW, BAT_CTRL, DISC, FLD, SPD, Ovl)
     comp_group <- comp_group %>% filter(comp_group$Ovl == comp_group[1,ncol(comp_group)])
     
-    comp_data <- comp_group[sample(1:nrow(comp_group),1),]
     
-    comp <- comp_data[1,1]
-    comp <- gsub("\\s*\\([^\\)]+\\)","", comp)
-    print(comp)
+    comp_data <- comp_group[sample(1:nrow(comp_group),1),]
+    mlb_comp <- rownames(comp_data)
+    #comp <- gsub("\\s*\\([^\\)]+\\)","", comp)
+    #print(comp)
     #perc_match <- ((500 - comp_data$Ovl) / 500) * 100
+    print(mlb_comp)
   })
+  
+  
   
   mlb_url = reactive({
     src = mlb_mugs['url'][mlb_mugs['name'] == mlb_comp()]
@@ -123,22 +131,80 @@ shinyServer(function(input, output){
   pros_url = reactive({
     src = pros_mugs['url'][pros_mugs['name'] == selected_player()]
   })
-
+  
+  
+  chart_data = reactive({
+    comp_tools <- mlb %>% filter(Name == mlb_comp())
+    
+    if (comp_tools$Pos.Class == 'Position'){
+      rownames(comp_tools) <- comp_tools$Name
+      comp_tools <- comp_tools %>% select(H, W, HIT, PWR, RAW, BAT_CTRL, DISC, FLD, SPD)
+    } else {
+      rownames(comp_tools) <- comp_tools$Name
+      comp_tools <- comp_tools %>% select(Name, H, W, FB, SL, CB, CH, CMD)
+    }
+    chart_data <- rbind(selected_data(), comp_tools) 
+    
+    chart_data <- t(chart_data)
+    chart_data <- data.frame(chart_data) #############################
+    print(chart_data)
+    colnames(chart_data) <- c(selected_player(), mlb_comp())
+    chart_data$Tool <- rownames(chart_data)
+    rownames(chart_data) <- NULL
+    
+    
+    pros_chart <- chart_data[,c(1,3)]
+    pros_chart$Player <- colnames(pros_chart)[1]
+    colnames(pros_chart)[1] <- 'Grade'
+    
+    comp_chart <- chart_data[,c(2,3)]
+    comp_chart$Player <- colnames(comp_chart)[1]
+    colnames(comp_chart)[1] <- 'Grade'
+    
+    chart_data <- rbind(pros_chart, comp_chart)
+  })
+  
+  #######################
+  # OUTPUTS
+  #######################
+  
   output$pros_mug <- renderText({c('<img src="',joe_random,'" height="110" width="80" align ="center">')})
-
+  
+  
   output$mlb_mug <- renderText({c('<img src="',mlb_url(),'" height="110" width="80" align ="center">')})
-
+  
+  
   output$prospect <- renderText(selected_player())
-
+  
+  
   output$comp <- renderText(mlb_comp())
 
+  
+  output$compPlot = renderPlot({
+    if (pros$Pos.Class[pros$Name == selected_player()] == 'Position'){
+      ggplot(chart_data(), aes(x=factor(Tool, levels=c('H', 'W', 'HIT', 'PWR', 'RAW', 'BAT_CTRL', 'DISC', 'FLD', 'SPD')), y=Grade, group=Player, fill=Player))+
+               geom_bar(position = 'dodge', stat = 'identity')+
+               scale_y_continuous(expand=c(0,0))+
+               xlab('Tools')+
+               theme_classic()
 
+    } else {
+      ggplot(chart_data(), aes(x=factor(Tool, levels=c('H', 'W', 'FB', 'SL', 'CB', 'CH', 'CMD')), y=Grade, group=Player, fill=Player))+
+        geom_bar(position = 'dodge', stat = 'identity')+
+        scale_y_continuous(expand=c(0,0))+
+        xlab('Tools')+
+        theme_classic()
+    }
+  })
+
+  
   output$card <- renderUI({
     name_url <-  gsub(" ","+", selected_player())
     url <- sprintf("https://www.ebay.com/sch/i.html?_from=R40&_nkw=%s+1st+bowman+chrome+auto+psa+10&_sacat=0&LH_Auction=1&_sop=1", name_url)
     tagList(a("ROOKIE CARD", href=url, target="_blank"))
   })
-
+  
+  
   output$highlight <- renderUI({
     name_url <-  gsub(" ","+", selected_player())
     url <- sprintf("https://www.youtube.com/results?search_query=%s+highlights+", name_url)
--- app/ui.R
+++ app/ui.R
@@ -8,57 +8,56 @@ library(shinydashboard)
 
 
 shinyUI(
-
+  
   fluidPage(
     
     titlePanel("MLB PROSPECT COMPS"),
-              column(12,
-                     br(),
-                     h4('Select a Prospect from Table Below and Click to Produce an MLB Comparison'),
-                     actionButton("compare", "Submit", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-                     br(),
-                     br(),
-                     br(),
-                     DTOutput("display_prospects")
-              ),
-              
+    column(12,
+           br(),
+           h4('Select a Prospect from Table Below and Click to Produce an MLB Comparison'),
+           actionButton("compare", "Submit", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+           br(),
+           br(),
+           br(),
+           DTOutput("display_prospects")
+    ),
+    
+    
+    bsModal("modal", "Big League Comps", trigger = 'compare', size = "large",
+            fluidRow(
+              column(4),
+              column(3, htmlOutput("pros_mug")),
+              column(3, htmlOutput("mlb_mug")),
+              column(2)
+            ),
+            br(),
+            fluidRow(
+              column(3),
+              column(3, verbatimTextOutput('prospect')),
+              column(3, verbatimTextOutput('comp')),
+              column(3)
+            ),
+            br(),
+            fluidRow(
+              column(4),
+              column(4, plotOutput("compPlot")),
+              column(4)
+            ),
+            br(),
+            fluidRow(
+              column(5),
+              column(4,uiOutput("card")),
+              column(3)
+            ),
+            br(),
+            fluidRow(
+              column(5),
+              column(4,uiOutput("highlight")),
+              column(3)
+            )
+    ),
+    h6("*Based on Frangraph's 2023 Preseason Top 100*")
+  )
+)
 
-              bsModal("modal", "Big League Comps", trigger = 'compare', size = "large",
-                      fluidRow(
-                        column(4),
-                        column(3, htmlOutput("pros_mug")),
-                        column(3, htmlOutput("mlb_mug")),
-                        column(2)
-                      ),
-                      br(),
-                      fluidRow(
-                        column(3),
-                        column(3, verbatimTextOutput('prospect')),
-                        column(3, verbatimTextOutput('comp')),
-                        column(3)
-                      ),
-                      br(),
-                      fluidRow(
-                        column(4),
-                        column(4,tableOutput("tbl")),
-                        column(4)
-                      ),
-                      br(),
-                      fluidRow(
-                        column(5),
-                        column(4,uiOutput("card")),
-                        column(3)
-                      ),
-                      br(),
-                      fluidRow(
-                        column(5),
-                        column(4,uiOutput("highlight")),
-                        column(3)
-                      )
-              ),
-              h6("*Based on Frangraph's 2023 Preseason Top 100*")
-          )
-      )
-  
-            
-        
+
